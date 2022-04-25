0e03194459ff4ebed3d8f4eab2005e30a588544c
Paul Ponzeka <ponzekap2@gmail.com>
2022-04-25 11:35:42 -0400

    commit

--- SipTestHTTPTrigger/run.ps1
+++ SipTestHTTPTrigger/run.ps1
@@ -2,7 +2,7 @@ using namespace System.Net
 
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
-
+##TEST Code
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
